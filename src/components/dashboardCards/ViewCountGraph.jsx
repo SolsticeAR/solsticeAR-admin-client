7e5e07cdf255c669942a65ea9aed41be0d799af1
Wil Andrade and julian <andradewilker+j.mourell@gmail.com>
2019-09-17 23:45:49 +0900

    graph skeleton, need data

--- src/components/dashboardCards/ViewCountGraph.jsx
+++ src/components/dashboardCards/ViewCountGraph.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
+import { trySetActiveMedia, setActiveMediaUrl } from "../../actions";
+import { connect } from "react-redux";
 
 // styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/App.css";
 
+// graph
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from "recharts";
+
 class ViewCountGraph extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const data = [
+      {
+        name: "Page A",
+        uv: 4000
+      },
+      {
+        name: "Page B",
+        uv: 3000
+      },
+      {
+        name: "Page C",
+        uv: 2000
+      },
+      {
+        name: "Page D",
+        uv: 2780
+      }
+    ];
+
     return (
       <div className="ViewCountGraph">
         <div className="card shadow mb-4">
@@ -16,9 +51,28 @@ class ViewCountGraph extends Component {
           </div>
 
           <div className="card-body">
-            <div className="chart-area">
-              <canvas id="myAreaChart"></canvas>
-            </div>
+            <AreaChart
+              width={700}
+              height={300}
+              data={data}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="uv"
+                stroke="#8884d8"
+                fill="#8884d8"
+              />
+            </AreaChart>
           </div>
         </div>
       </div>
@@ -26,4 +80,21 @@ class ViewCountGraph extends Component {
   }
 }
 
-export default ViewCountGraph;
+const mapStateToProps = state => {
+  return {
+    campaigns: state.reducer.campaigns
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setActiveMediaUrl: url => {
+      dispatch(setActiveMediaUrl(url));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ViewCountGraph);
