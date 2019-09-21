e65fbaf31b992e75148871b5a7becb1adf491e83
Julian Mourell <j.mourell@gmail.com>
2019-09-21 20:14:00 +0900

    Add support for previewing text and video

--- src/components/dashboardCards/ActiveExperience.jsx
+++ src/components/dashboardCards/ActiveExperience.jsx
@@ -11,20 +11,34 @@ class ActiveExperience extends Component {
   render() {
     return (
       <div className="ActiveExperience">
-        <div className="card shadow mb-4">
+        <div className="card shadow mb-4 ">
           <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
             <h6 className="m-0 font-weight-bold text-primary">
               Active AR Experience
             </h6>
           </div>
-
-          <div className="text-center">
+          {this.props.activeMediaObj && (
+          <div className="text-center ">
+            {this.props.activeMediaObj.type=== "image" && (
             <img
               className="img-fluid px-3 px-sm-4 mt-3 mb-4"
               src={this.props.activeMediaUrl}
               alt=""
-            />
+            /> )}
+             {this.props.activeMediaObj.type === "text" && (
+            <div
+              className=" px-3 px-sm-4 mt-3 mb-4"
+            > {this.props.activeMediaObj.name} </div>)}
+             { this.props.activeMediaObj.type === "video" && (
+            <div className="embed-responsive embed-responsive-4by3">
+            <video
+              className="embed-responsive-item px-3 px-sm-4 mt-3 mb-4"
+              src={this.props.activeMediaUrl}
+              alt={this.props.activeMediaObj.name}
+            /></div> )}
+            
           </div>
+          )}
         </div>
       </div>
     );
@@ -34,7 +48,8 @@ class ActiveExperience extends Component {
 const mapStateToProps = state => {
   return {
     campaigns: state.reducer.campaigns,
-    activeMediaUrl: state.reducer.activeMediaUrl
+    activeMediaUrl: state.reducer.activeMediaUrl,
+    activeMediaObj :state.reducer.activeMediaObj
   };
 };
 
