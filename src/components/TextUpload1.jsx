d94d5bbd99ad6d32e38e6cabb228c0acfacea2dd
frank <frank.m.fernandez@gmail.com>
2019-09-23 15:01:30 +0900

    updates

--- src/components/TextUpload1.jsx
+++ src/components/TextUpload1.jsx
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-<<<<<<< HEAD
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
-import { createNewMedia } from "../actions/index";
-
-
-=======
 import { createNewMedia } from "../actions/index";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 import "../styles/App.css"
->>>>>>> 6cbf69aa86fe6a41af82a45ead79afbea794aec4
 
 class TextUpload1 extends Component {
   constructor(props) {
@@ -18,16 +11,11 @@ class TextUpload1 extends Component {
       upload: ""
     };
   }
-<<<<<<< HEAD
-  handleClick(e) {
-      console.log("this is upload",this.state.upload)
-=======
   closeForm(){
     document.getElementById('messageForm').style.display = "none"
     document.getElementById('overlay').style.display = "none"
   }
   handleClick(e) {
->>>>>>> 6cbf69aa86fe6a41af82a45ead79afbea794aec4
     e.preventDefault();
     const media = {
       name: this.state.upload,
@@ -35,45 +23,6 @@ class TextUpload1 extends Component {
       type: "text",
       campaignId: 1 // Set to default 1st campaign
     };
-<<<<<<< HEAD
-    this.props.createNewMedia(media);
-  }
-  render() {
-    return (
-    //   <a className="nav-link" href="#home">
-    //     <i className="fas fa-fw fa-tachometer-alt"></i>
-    //     <span>Upload Text</span>
-    //     <input
-    //       type="text"
-    //       className="form-control form-control-user"
-    //       id="textUpload"
-    //       placeholder="Type your text here "
-    //       onChange={e => this.setState({ upload: e.target.value })}
-    //     />
-    //     <button
-    //       className="btn btn-primary btn-user btn-block"
-    //       onClick={e => this.handleClick(e)}
-    //     >
-    //       Upload Text
-    //     </button>
-    //   </a>
-
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form>
-            <p className="h4 text-center mb-4">Compose Message</p>
-            <label htmlFor="defaultFormContactNameEx" className="grey-text">
-              Your name
-            </label>
-            <input
-              type="text"
-              id="defaultFormContactNameEx"
-              className="form-control"
-            />
-            <br />
-=======
-    console.log("this is state upload", this.state.upload)
     this.props.createNewMedia(media);
     this.closeForm()
   }
@@ -88,7 +37,6 @@ class TextUpload1 extends Component {
         <MDBCol md="6" id="formColumn">
           <form >
             <p className="h4 text-center">Compose Message</p>
->>>>>>> 6cbf69aa86fe6a41af82a45ead79afbea794aec4
             <label
               htmlFor="defaultFormContactSubjectEx"
               className="grey-text"
@@ -115,27 +63,18 @@ class TextUpload1 extends Component {
               onChange={e => this.setState({ upload: e.target.value })}
             />
             <div className="text-center mt-4">
-<<<<<<< HEAD
-              <MDBBtn color="warning" outline type="submit" onClick={e => this.handleClick(e)}>
-                Send
-                <MDBIcon far icon="paper-plane" className="ml-2" />
-=======
               <MDBBtn class="messageFormButton"  outline type="submit" onClick={e => this.handleClick(e)}>
                 Send
               </MDBBtn>
               <MDBBtn class="messageFormButton"  outline onClick={()=>this.closeForm()}>
                 Cancel
->>>>>>> 6cbf69aa86fe6a41af82a45ead79afbea794aec4
               </MDBBtn>
             </div>
           </form>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
-<<<<<<< HEAD
-=======
     </div>
->>>>>>> 6cbf69aa86fe6a41af82a45ead79afbea794aec4
     );
   }
 }
