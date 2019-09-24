7e9fd1a6c026aadc5ba41c625be4557391a81f32
wilandrade <wilandrade@Wils-MacBook-Pro.local>
2019-09-24 12:21:21 +0900

    Check for authdata before redirecting to login page.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -24,7 +24,7 @@ class Register extends Component {
 
   componentDidMount() {
     const authData = getAuthTokenFromLS();
-    this.props.redirectToLogin();
+    if (authData) this.props.redirectToLogin();
   }
 
   handleSubmit(e) {
